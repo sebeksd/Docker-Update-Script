87b32ed080bb30bc562c62195d28772d0583560e
sebeksd <18705953+sebeksd@users.noreply.github.com>
2024-07-28 13:01:59 +0200

    In case of different container states (running/stopped) make script more predictable, stopped containers will be updated but not started Report which containers are stopped in Updated and Not Updated summaries Normalize 'then' formating

--- Update.sh
+++ Update.sh
@@ -23,6 +23,11 @@ With yaml file name as parameter only that container will be created/pulled/recr
 Script will: 
   - REBUILD custom images (with correct pair of compose '*.yaml' file and '*.dockerfile' file) 
   - use ENV file (with correct pair of compose '*.yaml' file and '*.env' file) 
+
+Depending on container state:
+ - if does not exist => create and start
+ - if exists but not running => update but leave stopped
+ - if exists and running => recreate and restart
   
 Additional options are:
   -c - check only, it will pull images without updating running containers, in this mode there is no cleanup (ignores -r0, -r1, -r2, -rv, -ri, -po) 
@@ -80,16 +85,14 @@ do
 done
 
 # Sanity check for parameters
-if [[ "$vNoUpdate" == true && "$vUpdatePulledOnly" == true ]]; 
-then  
+if [[ "$vNoUpdate" == true && "$vUpdatePulledOnly" == true ]]; then  
   # in pull only ignore "no pull" flag 
   vUpdatePulledOnly=false
   echo -e "${cRED}Do not use -c with -po, ignoring -po${cNC}"
 fi
 
 # Update ALL or single Container
-if [ -z "$vInputFiles" ] 
-then
+if [ -z "$vInputFiles" ]; then
   echo -e "${cBlue}Updating ${cGreen}ALL${cBlue}, Working directory is ${cGreen}'$cSCRIPT_DIR'${cNC}"
   vInputFiles=$cSCRIPT_DIR/*.yaml
   vSingleFileMode=false
@@ -111,23 +114,39 @@ else
   vSingleFileMode=true
 fi
 
+container_exists()
+{
+  if [ "$(docker ps -aq -f name=$vFilename)" ]; then
+    true # exists
+  else
+    false # not exists
+  fi  
+}
+
+container_running()
+{
+  # Warning! correct result is raported only if container exists 
+  if [ "$(docker ps -aq -f status=exited -f status=created -f name=$vFilename)" ]; then
+    false # stopped and existing
+  else
+    true # running or not existing
+  fi   
+}
+
 docker_cleanup()
 {
-  if [[ "$vCleanupLevel" == 0 || "$vNoUpdate" == true ]]; 
-  then
+  if [[ "$vCleanupLevel" == 0 || "$vNoUpdate" == true ]]; then
     return
   fi
 
   echo -e "${cBlue}## Cleanup ##${cNC}"
 
-  if [ "$vCleanupOnlyVolumes" == false ]; 
-  then  
+  if [ "$vCleanupOnlyVolumes" == false ]; then  
     echo -e "${cBlue}## Removing old images ##${cNC}"
     docker image prune -a -f
   fi
 
-  if [ "$vCleanupOnlyImages" == false ]; 
-  then
+  if [ "$vCleanupOnlyImages" == false ]; then
     echo -e "${cBlue}## Removing old volumes ##${cNC}"
     docker volume prune -f
   fi
@@ -135,32 +154,25 @@ docker_cleanup()
 }
 
 vModes="${cBlue}Mode:${cNC}\n"
-if [ "$vNoUpdate" == true ]; 
-then
+if [ "$vNoUpdate" == true ]; then
   vModes="$vModes ${cYellow}  - Pull only, not updating!${cNC}\n"
 else
-  if [ "$vCleanupLevel" == 0 ]; 
-  then
+  if [ "$vCleanupLevel" == 0 ]; then
     vModes="${cYellow}  - No cleanup${cNC}\n\n"
   else
-    if [ "$vCleanupLevel" == 1 ]; 
-    then
+    if [ "$vCleanupLevel" == 1 ]; then
       vModes="$vModes ${cYellow}  - Cleanup on script finish${cNC}\n"
     fi
-    if [ "$vCleanupLevel" == 2 ]; 
-    then
+    if [ "$vCleanupLevel" == 2 ]; then
       vModes="$vModes ${cYellow}  - Cleanup after every update${cNC}\n"
     fi
-    if [ "$vCleanupOnlyImages" == true ]; 
-    then
+    if [ "$vCleanupOnlyImages" == true ]; then
       vModes="$vModes ${cYellow}  - Cleanup only Images${cNC}\n"
     fi
-    if [ "$vCleanupOnlyVolumes" == true ]; 
-    then
+    if [ "$vCleanupOnlyVolumes" == true ]; then
       vModes="$vModes ${cYellow}  - Cleanup only Volumes${cNC}\n"
     fi
-    if [ "$vUpdatePulledOnly" == true ]; 
-    then
+    if [ "$vUpdatePulledOnly" == true ]; then
       vModes="$vModes ${cYellow}  - Do not pull new images, update only already pulled${cNC}\n"
     fi
   fi
@@ -174,19 +186,16 @@ do
   vFilenameLower=$(echo $vFilename | tr '[:upper:]' '[:lower:]') # docker-compose do not like upper case in stack name
   vEnvFilename="$cSCRIPT_DIR/${vFilename}.env"
 
-  if [[ $vFilename == $cSkipChar* ]]; 
-  then
+  if [[ $vFilename == $cSkipChar* ]]; then
     vFilename=${vFilename/#$cSkipChar}
-    if [[ "$vSingleFileMode" = false ]]; 
-    then
-      vSKIPPED="$vSKIPPED  $vFilename \n"
+    if [[ "$vSingleFileMode" = false ]]; then
+      vSKIPPED="$vSKIPPED  ${cYellow}$vFilename${cNC}\n"
       #echo -e "${cBlue}######################## Skipping ${cYellow}$vFilename ${cBlue}########################${cNC}\n"
       continue 
     fi
   fi
 
-  if [ "$vNoUpdate" = false ]; 
-  then
+  if [ "$vNoUpdate" = false ]; then
     echo -e "${cBlue}######################## Updating ${cGreen}$vFilename ${cBlue}########################${cNC}"
   else
     echo -e "${cBlue}######################## Checking/Pulling ${cGreen}$vFilename ${cBlue}########################${cNC}"
@@ -200,8 +209,7 @@ do
   fi
 
   # skipp pulling images when option -po was provided
-  if [[ "$vUpdatePulledOnly" == false ]]; 
-  then  
+  if [[ "$vUpdatePulledOnly" == false ]]; then
     echo -e "${cBlue}## Checking for new images / build / pull ##${cNC}"
     vImagesCountPre=$(docker images -q | wc -l)
     docker-compose -f $vFile -p $vFilenameLower $vUseEnv build --pull
@@ -214,48 +222,96 @@ do
     vExitCode_PULL=0
   fi
 
-  # create or recreate/update container (all comand output catched to variable for further use)
-  if [ "$vNoUpdate" = false ]; 
-  then
+  vContainerExists=false
+  vContainerRunning=false
+
+  # create or recreate/update container (all command output catched to variable for further use)
+  if [ "$vNoUpdate" = false ]; then
     echo -e "${cBlue}## Upgrade if needed ##${cNC}"
-    vDOCKER_UP=$(docker-compose -f $vFile -p $vFilenameLower $vUseEnv up -d 2>&1) 
-      vExitCode_UP=$? # save exit code for later
+    # Checking if container exists and is running
+    # - if does not exist => create and start
+    # - if exists but not running => update but leave stopped
+    # - if exists and running => recreate and start
+    vShouldStart=""
+
+    if container_exists; then
+      vContainerExists=true
+      
+      if ! container_running; then
+        vShouldStart="--no-start"
+        echo -e "${cYellow} Container is not running, updating only${cNC}"
+      else
+        vContainerRunning=true
+      fi
+    else
+      echo -e "${cYellow} Container does not exist, trying to Create${cNC}"
+    fi
+
+    vDOCKER_UP=$(docker-compose -f $vFile -p $vFilenameLower $vUseEnv up $vShouldStart -d 2>&1) 
+    vExitCode_UP=$? # save exit code for later
+
     echo "$vDOCKER_UP" # all output was catched to variable so we need to write it to terminal
   else
     vExitCode_UP=0
   fi
 
   # if any of the commands returne error exit code than report it
-  if [[ $vExitCode_BUILD != 0 || $vExitCode_PULL != 0 || $vExitCode_UP != 0 ]];
-  then
-    vERROR="$vERROR  $vFilename \n"
+  if [[ $vExitCode_BUILD != 0 || $vExitCode_PULL != 0 || $vExitCode_UP != 0 ]]; then
+    vERROR="$vERROR  ${cRED}$vFilename${cNC}\n"
     continue
   fi
 
-  if [ "$vNoUpdate" = true ]; 
-  then
+  if [ "$vNoUpdate" = true ]; then
     # parsing and displaying output of docker-compose pull at the same time is hard so instead
     # we count docker images before and after
-    if [[ $vImagesCountPre < $vImagesCountPost ]]; 
-    then
+    if [[ $vImagesCountPre < $vImagesCountPost ]]; then
       vUPDATED="$vUPDATED  $vFilename \n"
     else
       vNOT_UPDATED="$vNOT_UPDATED  $vFilename \n"
     fi
   else
-    # if no update was made skip (yes I know that single expression would be enough)
-    if [[ $vDOCKER_UP == *"Creat"* || $vDOCKER_UP == *"Recreat"* ]]; 
-    then
-      vUPDATED="$vUPDATED  $vFilename \n"
-      
+    # if no update was made skip
+    # depending on state of the container before update we parse output differently
+    vWasUpdated=false
+
+    # container does not exists
+    if [ "$vContainerExists" = false ]; then
+      if [[ $vDOCKER_UP == *"Creat"* ]]; then
+        vWasUpdated=true
+      fi
+    fi
+
+    # container exists but not running
+    if [[ "$vContainerExists" = true && "$vContainerRunning" = false ]]; then
+      if [[ $vDOCKER_UP == *"Recreat"* ]]; then
+        vWasUpdated=true
+      fi
+    fi
+
+    # container exists and is running
+    if [[ "$vContainerExists" = true && "$vContainerRunning" = true ]]; then
+      if [[ $vDOCKER_UP == *"Recreat"* ]]; then
+        vWasUpdated=true
+      fi
+    fi
+
+    if [ "$vWasUpdated" = true ]; then
+      if [ "$vContainerRunning" = true ]; then
+        vUPDATED="$vUPDATED  ${cGreen}$vFilename${cNC}\n"
+      else
+        vUPDATED="$vUPDATED  ${cGreen}$vFilename${cNC} - ${cYellow}stopped${cNC}\n"
+      fi
       # cleanup after every update if enabled
       # to make sure we have enaugh free space remove all old images after every update
-      if [ "$vCleanupLevel" = 2 ]; 
-      then
+      if [ "$vCleanupLevel" = 2 ]; then
         docker_cleanup
       fi
     else
-      vNOT_UPDATED="$vNOT_UPDATED  $vFilename \n"
+      if [ "$vContainerRunning" = true ]; then
+        vNOT_UPDATED="$vNOT_UPDATED  ${cGreen}$vFilename${cNC}\n"
+      else
+        vNOT_UPDATED="$vNOT_UPDATED  ${cGreen}$vFilename${cNC} - ${cYellow}stopped${cNC}\n"
+      fi
     fi
   fi
 
@@ -263,8 +319,7 @@ do
 done
 
 # cleanup on script finish if enabled
-if [[ "$vCleanupLevel" = 1 || "$vCleanupLevel" = 2 ]]; 
-then
+if [[ "$vCleanupLevel" = 1 || "$vCleanupLevel" = 2 ]]; then
   docker_cleanup
 fi
 
@@ -272,42 +327,37 @@ fi
 echo -e "${cBlue}########################${cGreen} Done ${cBlue}########################${cNC}\n"
 
 # Summary for user plus LOG file
-if [ "$vNoUpdate" = false ]; 
-then
+if [ "$vNoUpdate" = false ]; then
   vSUMMARY="$vSUMMARY${cBlue}Created or updated:${cNC}\n"
 else
   vSUMMARY="$vSUMMARY${cBlue}Updates available:${cNC}\n"
 fi
 
-if [ -z "$vUPDATED" ]
-then
-      vSUMMARY="$vSUMMARY [none]\n\n"
+if [ -z "$vUPDATED" ]; then
+      vSUMMARY="$vSUMMARY  [none]\n\n"
 else
-      vSUMMARY="$vSUMMARY${cGreen}$vUPDATED${cNC}\n"
+      vSUMMARY="$vSUMMARY$vUPDATED\n"
 fi
 
 vSUMMARY="$vSUMMARY${cBlue}Already up-to-date:${cNC}\n"
-if [ -z "$vNOT_UPDATED" ]
-then
-      vSUMMARY="$vSUMMARY [none]\n\n"
+if [ -z "$vNOT_UPDATED" ]; then
+      vSUMMARY="$vSUMMARY  [none]\n\n"
 else
-      vSUMMARY="$vSUMMARY${cGreen}$vNOT_UPDATED${cNC}\n"
+      vSUMMARY="$vSUMMARY$vNOT_UPDATED\n"
 fi
 
 vSUMMARY="$vSUMMARY${cBlue}SKIPPED:${cNC}\n"
-if [ -z "$vSKIPPED" ]
-then
-      vSUMMARY="$vSUMMARY [none]\n\n"
+if [ -z "$vSKIPPED" ]; then
+      vSUMMARY="$vSUMMARY  [none]\n\n"
 else
-      vSUMMARY="$vSUMMARY${cYellow}$vSKIPPED${cNC}\n"
+      vSUMMARY="$vSUMMARY$vSKIPPED\n"
 fi
 
 vSUMMARY="$vSUMMARY${cBlue}ERROR:${cNC}\n"
-if [ -z "$vERROR" ]
-then
-      vSUMMARY="$vSUMMARY [none]\n\n"
+if [ -z "$vERROR" ]; then
+      vSUMMARY="$vSUMMARY  [none]\n\n"
 else
-      vSUMMARY="$vSUMMARY${cRED}$vERROR${cNC}\n"
+      vSUMMARY="$vSUMMARY$vERROR\n"
 fi
 
 # Log file with date and summary
